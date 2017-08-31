d6010685798a8a0aeadea48f9f45d154f5c2c2eb
ASUS <ASUS@asus-pc>
2017-08-31 22:22:43 +0800

    https://github.com/matt19870107/spring-boot-autoinject.git

---
+++ src/main/java/com/autoinject/spring_boot_autoinject/App.java
@@ -0,0 +1,13 @@
+package com.autoinject.spring_boot_autoinject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/autoinject/spring_boot_autoinject/HelloServiceAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.autoinject.spring_boot_autoinject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(HelloServiceProperties.class)
+@ConditionalOnClass(HelloService.class)
+@ConditionalOnProperty(prefix="hello",value="enabled")
+public class HelloServiceAutoConfiguration {
+
+	@Autowired
+	private HelloServiceProperties helloServiceProperties;
+	
+	public HelloService getHelloService() {
+		HelloService helloService = new HelloService();
+		helloService.setMsg(helloServiceProperties.getMsg());
+		return helloService;
+	}
+}
